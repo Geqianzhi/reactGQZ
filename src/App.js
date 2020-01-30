aedcd456b8853cc11781ac70658cd90722315c16
戈乾志 <geqianzhi@sinoecare.com>
2020-01-30 10:54:59 +0800

    app

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Button, Pagination} from 'antd';
 function App() {
   return (
     <div >
-      <Button>拜托你</Button>
+      <Button>夏天凿冰吃瓜，冬天围炉取暖</Button>
       <Pagination showQuickJumper defaultCurrent={2} total={500}  />
     </div>
   );
