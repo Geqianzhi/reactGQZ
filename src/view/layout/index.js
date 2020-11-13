b8e73cb8af39b8d9d0a8482444e7f13bb047e337
戈乾志 <976320797@qq.com>
2020-11-13 17:43:01 +0800

    layout

--- src/view/layout/index.js
+++ src/view/layout/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {Layout} from 'antd';
 import './layout.less';
+import {
+    MenuUnfoldOutlined,
+    MenuFoldOutlined
+} from '@ant-design/icons';
 const {Header,Sider,Content} = Layout;
 export default function LayoutPage() {
+    const [collapsed,setCollapsed] = useState(false);
+    const setColl = (val)=>{
+        val ? setCollapsed(false):setCollapsed(true)
+    }
     return (
         <>
             <Layout className="layout">
-                <Sider>Sider</Sider>
+                <Sider collapsible collapsed={collapsed}>Sider</Sider>
                 <Layout>
-                    <Header>Header</Header>
+                    <Header className="header">
+                        <div className="collapsed-icon" onClick={() => setColl(collapsed)}>
+                            {collapsed ? <MenuUnfoldOutlined/> : <MenuFoldOutlined/> }
+                        </div>
+                    </Header>
                     <Content>Content</Content>
                 </Layout>
             </Layout>
