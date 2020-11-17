5fa9f8ec240ce910ac4833632c9db69f1cf14be3
戈乾志 <976320797@qq.com>
2020-11-17 17:27:49 +0800

    mobx

--- src/Mobx/index.js
+++ src/Mobx/index.js
@@ -7,7 +7,15 @@ class state {
 
     @action
     changeRoutes = route =>{
-        this.activeRoutes.push({...route})
+        if(this.activeRoutes.length>0){
+            let flag = this.activeRoutes.some(e => e.name == route.name);
+            if(!flag){
+                this.activeRoutes.push({...route});
+            }
+            
+        }else{
+            this.activeRoutes.push({...route});
+        }
     }
 }
 const store = new state();
