a2a96ea9b60d2bf8db88ea54b19cdcb9e7e93925
戈乾志 <976320797@qq.com>
2020-11-20 11:40:07 +0800

    css3贝赛尔曲线

--- src/view/layout/index.js
+++ src/view/layout/index.js
@@ -39,8 +39,8 @@ function LayoutPage(props) {
                                     const childrens = e.childrens;
                                     let list = childrens.map((item,i)=>{
                                         return (
-                                            <SubMenu key={index} icon={item.icon} title={e.name}>
-                                                <Menu.Item key={`${index}-${i}`}>
+                                            <SubMenu key={index} icon={e.icon} title={e.name}>
+                                                <Menu.Item key={`${index}-${i}`} icon={item.icon}>
                                                     <NavLink to={item.path} onClick={()=>selectRoute(item)}>{item.name}</NavLink>
                                                 </Menu.Item>
                                             </SubMenu>
