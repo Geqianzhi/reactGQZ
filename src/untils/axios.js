e922f75d5fa8db9d7df8e45063faa21c9dc3e2ac
戈乾志 <976320797@qq.com>
2020-11-30 15:10:29 +0800

    axios封装

--- src/untils/axios.js
+++ src/untils/axios.js
@@ -29,4 +29,39 @@ instance.interceptors.response.use(function (response) {
     // 对响应错误做点什么
     return Promise.reject(error);
 });
+
+export function get(url,params={}){
+    return new Promise((reslove,reject)=>{
+        instance.get(url,{
+            params
+        }).then(res =>{
+            reslove(res)
+        }).catch(error =>{
+            reject(error)
+        })
+    })
+}
+
+export function post(url,data,type='body'){
+    return new Promise((reslove ,reject)=>{
+        if(type==='body'){
+            instance.post(url,{...data})
+            .then(res =>{
+                reslove(res)
+            }).catch(error =>{
+                reject(error)
+            })
+        }
+        if(type==='params'){
+            instance.post(url,null,{
+                params:data
+            })
+            .then(res =>{
+                reslove(res)
+            }).catch(error =>{
+                reject(error)
+            })
+        }   
+    })
+}
 export default instance;
